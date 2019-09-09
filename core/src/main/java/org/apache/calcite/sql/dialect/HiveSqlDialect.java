dc7aca90da9ad87038af4d756256c0d0db1a34e4
sachin.rohaj <sachin.rohaj@datametica.com>
2019-09-09 18:51:54 +0530

    Missed changes for format function in HIVE and Spark dialect after cherry pickings.

--- core/src/main/java/org/apache/calcite/sql/dialect/HiveSqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/dialect/HiveSqlDialect.java
@@ -162,6 +162,8 @@ public class HiveSqlDialect extends SqlDialect {
     case DIVIDE_INTEGER:
       unparseDivideInteger(writer, call, leftPrec, rightPrec);
       break;
+    case FORMAT:
+      unparseFormat(writer, call, leftPrec, rightPrec);
     default:
       super.unparseCall(writer, call, leftPrec, rightPrec);
     }
