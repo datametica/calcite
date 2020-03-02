e17a0dc9aafa89d21a9e33df7819bab7d8142856
Deepak Sharma <deepak.sharma@datametica.com>
2020-03-02 18:59:47 +0530

    Fix test cases failure for snowflake

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -3686,7 +3686,10 @@ public class RelToSqlConverterTest {
         + "FROM (SELECT 1 AS a, 'x ' AS b\n"
         + "UNION ALL\n"
         + "SELECT 2 AS a, 'yy' AS b)";
-    final String expectedSnowflake = expectedPostgresql;
+    final String expectedSnowflake = "SELECT \"a\"\n"
+        + "FROM (SELECT 1 AS \"a\", 'x ' AS \"b\"\n"
+        + "UNION ALL\n"
+        + "SELECT 2 AS \"a\", 'yy' AS \"b\")";
     final String expectedRedshift = expectedPostgresql;
     sql(sql)
         .withHsqldb()
