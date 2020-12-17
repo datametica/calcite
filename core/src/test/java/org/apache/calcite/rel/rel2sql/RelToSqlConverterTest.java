68794af262882dd28408d80355309359bae6b1a8
Deepak Sharma <deepak.sharma@datametica.com>
2020-12-17 19:33:07 +0530

    Cherry pick from 2020-10-01 to 2020-12-31

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -9679,6 +9679,10 @@ class RelToSqlConverterTest {
         + "CAST(FROM_UNIXTIME(UNIX_TIMESTAMP('2018/11/06', 'yyyy/MM/dd'), 'yyyy-MM-dd') AS DATE) date2\n"
         + "FROM scott.EMP";
     final String expectedSpark = expectedHive;
+    final String expectedSnowflake =
+        "SELECT TO_DATE('20181106', 'YYYYMMDD') AS \"date1\", "
+        + "TO_DATE('2018/11/06', 'YYYY/MM/DD') AS \"date2\"\n"
+        + "FROM \"scott\".\"EMP\"";
     assertThat(toSql(root, DatabaseProduct.CALCITE.getDialect()), isLinux(expectedSql));
     assertThat(toSql(root, DatabaseProduct.BIG_QUERY.getDialect()), isLinux(expectedBiqQuery));
     assertThat(toSql(root, DatabaseProduct.HIVE.getDialect()), isLinux(expectedHive));
