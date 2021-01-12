ff864d85a7761809c6408436bd2da7688c41fae0
sounak.roy <sounak.roy@datametica.com>
2021-01-12 20:28:39 +0530

    RAV-4558 Resolve : Added Hive & Spark Changes

--- core/src/main/java/org/apache/calcite/sql/dialect/HiveSqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/dialect/HiveSqlDialect.java
@@ -146,13 +146,10 @@ public class HiveSqlDialect extends SqlDialect {
     super(context);
     // Since 2.1.0, Hive natively supports "NULLS FIRST" and "NULLS LAST".
     // See https://issues.apache.org/jira/browse/HIVE-12994.
-    emulateNullDirection = (context.databaseMajorVersion() < 2)
-        || (context.databaseMajorVersion() == 2
-        && context.databaseMinorVersion() < 1);
-
-    isHiveLowerVersion = (context.databaseMajorVersion() < 2)
-        || (context.databaseMajorVersion() == 2
-        && context.databaseMinorVersion() < 1);
+    emulateNullDirection = (context.databaseMajorVersion() <= 2)
+      && context.databaseMinorVersion() < 1;
+    isHiveLowerVersion = (context.databaseMajorVersion() <= 2)
+      && context.databaseMinorVersion() < 1;
   }
 
   @Override protected boolean allowsAs() {
@@ -193,7 +190,6 @@ public class HiveSqlDialect extends SqlDialect {
     if (emulateNullDirection) {
       return emulateNullDirectionWithIsNull(node, nullsFirst, desc);
     }
-
     return null;
   }
 
