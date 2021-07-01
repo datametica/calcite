94a1a3726ffbfc7f0119f2e732c00a2d4ded5718
saurabh.gupta <saurabh.gupta@datametica.com>
2021-07-01 13:16:47 +0530

    changing variable name

--- core/src/main/java/org/apache/calcite/rel/rel2sql/RelToSqlConverter.java
+++ core/src/main/java/org/apache/calcite/rel/rel2sql/RelToSqlConverter.java
@@ -360,7 +360,7 @@ public class RelToSqlConverter extends SqlImplementor
     parseCorrelTable(e, x);
     final Builder builder = x.builder(e);
     if (!isStar(e.getProjects(), e.getInput().getRowType(), e.getRowType())
-        || style.isExpandStar()) {
+        || style.isExpandProjection()) {
       final List<SqlNode> selectList = new ArrayList<>();
       for (RexNode ref : e.getProjects()) {
         SqlNode sqlExpr = builder.context.toSql(null, ref);
