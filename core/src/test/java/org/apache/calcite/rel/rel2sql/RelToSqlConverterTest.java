c214e64d7c65863a4ecb32166b7cbedbf9a9252b
Kowsikraja.R <kowsikraja.r@datametica.com>
2023-09-09 12:49:25 +0530

    Commit 2

--- core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
+++ core/src/test/java/org/apache/calcite/rel/rel2sql/RelToSqlConverterTest.java
@@ -11533,7 +11533,7 @@ class RelToSqlConverterTest {
   }
 
 
-  @Test public void testNextDayFunctionWithDate() {
+  @Test public void testNextDayFunctionWithDateTest() {
     final RelBuilder builder = relBuilder();
     final RexNode nextDayRexNode = builder.call(SqlLibraryOperators.NEXT_DAY,
         builder.literal("2023-02-22"), builder.literal(DayOfWeek.TUESDAY.name()));
