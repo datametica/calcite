a440a39a1615cf21505270ae8bf5854814ed0170
Kowsikraja.R <kowsikraja.r@datametica.com>
2023-12-18 14:37:14 +0530

    Adding some logic in REGEXP_REPLACE function

--- core/src/main/java/org/apache/calcite/sql/dialect/BigQuerySqlDialect.java
+++ core/src/main/java/org/apache/calcite/sql/dialect/BigQuerySqlDialect.java
@@ -1699,7 +1699,7 @@ public class BigQuerySqlDialect extends SqlDialect {
 
   private void unparseRegexpReplace(SqlWriter writer, SqlCall call,
       int leftPrec, int rightPrec) {
-    int indexOfRegexOperand = 1;
+    int indexOfRegexOperand = 0;
     SqlWriter.Frame regexpReplaceFrame = writer.startFunCall("REGEXP_REPLACE");
     List<SqlNode> operandList = call.getOperandList();
     unparseRegexFunctionsOperands(writer, leftPrec, rightPrec, indexOfRegexOperand, operandList);
@@ -1722,6 +1722,7 @@ public class BigQuerySqlDialect extends SqlDialect {
       if (operandList.indexOf(operand) == indexOfRegexOperand
           && operand instanceof SqlCharStringLiteral) {
         unparseRegexLiteral(writer, operand);
+        indexOfRegexOperand++;
       } else {
         operand.unparse(writer, leftPrec, rightPrec);
       }
